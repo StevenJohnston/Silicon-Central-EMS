d3f462f1087d22da6e7f8eec06131451c0d58fc9
Williampring <mrwilliampring@gmail.com>
2015-11-10 15:16:31 -0800

    Added the ValidateDateOfBirth function Validate the birthday

--- AllEmployees/Employee.cs
+++ AllEmployees/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,5 +13,38 @@ namespace AllEmployees
         protected string lastName;
         protected string socialInsuranceNumber;
         protected string dateOfBirth;
+
+        string ValidateDateOfBirth(string dateOfBirth)
+        {
+            int year = 0;
+            CultureInfo culture;
+            culture = CultureInfo.CreateSpecificCulture("en-US");
+            string newTemp = dateOfBirth.ToLower();
+            string formats = "yyyy/mm/dd";
+            DateTime dateValue;
+
+            newTemp = newTemp.Remove(4, newTemp.Length - 4);
+            if (DateTime.TryParseExact(newTemp, formats, new CultureInfo("en-US"), DateTimeStyles.None, out dateValue))
+            {
+                //convert string to int
+                year = Convert.ToInt32(newTemp);
+                //check year range
+                if ((year >= 1900) && (year <= 2001))
+                {
+
+                }
+                else
+                {
+
+                }
+
+            }
+            else
+            {
+
+            }
+            return "";
+
+        }
     }
 }
