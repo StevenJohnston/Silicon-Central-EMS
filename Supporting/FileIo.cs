d7876c3d79ce6a16bec8ff91305cabfbb6f8a1ed
Steven Johnston <wheatthins@hotmail.ca>
2015-11-12 11:59:44 -0500

    Added Load Method

--- Supporting/FileIo.cs
+++ Supporting/FileIo.cs
@@ -1,12 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Supporting
 {
+    public struct Message
+    {
+        public int code;
+        public string message;
+    }
     public class FileIO
     {
+        public delegate Message loadFunc(object param);
+        public static object lockObj = new object();
+        string path = "DBase/EMS project DBase.txt";
+        public FileIO()
+        {
+            
+        }
+        public Message Load(loadFunc func)
+        {
+            Message returnMessage = new Message();
+            returnMessage.code = 200;
+            lock(lockObj)
+            {
+                try
+                {
+                    using (StreamReader fileIn = new StreamReader(path))
+                    {
+                        func(fileIn.ReadLine());
+                    }
+                }
+                catch (Exception e)
+                {
+
+                }
+                finally
+                {
+
+                }
+            }
+            return returnMessage;
+        }
+        public Message write(loadFunc func, object param)
+        {
+            return new Message();
+        } 
     }
 }
