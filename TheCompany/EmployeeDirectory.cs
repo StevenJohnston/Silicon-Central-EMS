1c6e8156dc18414f689ce8ac1da75de6e6d90174
Steven johnston <wheatthins@hotmail.ca>
2015-11-11 22:48:00 -0500

    Using AllEmployees and Supporting Libraries

--- TheCompany/EmployeeDirectory.cs
+++ TheCompany/EmployeeDirectory.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using AllEmployees;
+using Supporting;
 namespace TheCompany
 {
     public class EmployeeDirectory
     {
+        //key: sin
+        Dictionary<string,Employee> employees = new Dictionary<string, Employee>();
+        FileIO file = new FileIO();
+        public void Load()
+        {
+
+        }
     }
 }
