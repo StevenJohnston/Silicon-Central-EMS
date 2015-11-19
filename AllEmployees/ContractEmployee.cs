5e02b7de5bc531714df3c8c5a04e31892a59ea39
Matthew Warren <warren.matt@hotmail.com>
2015-11-19 13:22:48 -0500

    Added ValidateDate(string, dateType), specific to ContractEmployee

--- AllEmployees/ContractEmployee.cs
+++ AllEmployees/ContractEmployee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,14 @@ namespace AllEmployees
 {
     public class ContractEmployee : Employee
     {
-        string contractStartDate;
-        string contractStopDate;
+        DateTime contractStartDate;
+        DateTime contractStopDate;
         decimal fixedContractAmount;
+
+        public ContractEmployee()
+        {
+
+        }
 
         public ContractEmployee(string[] employeeData)
         {
@@ -21,9 +27,9 @@ namespace AllEmployees
                 firstName = employeeData[index];
                 lastName = employeeData[index + 1];
                 socialInsuranceNumber = employeeData[index + 2];
-                dateOfBirth = employeeData[index + 3];
-                contractStartDate = employeeData[index + 4];
-                contractStopDate = employeeData[index + 5];
+                dateOfBirth = Convert.ToDateTime(employeeData[index + 3]);
+                contractStartDate = Convert.ToDateTime(employeeData[index + 4]);
+                contractStopDate = Convert.ToDateTime(employeeData[index + 5]);
                 fixedContractAmount = Convert.ToDecimal(employeeData[index + 6]);
             }
         }
@@ -33,8 +39,12 @@ namespace AllEmployees
             bool allValid = false;
             bool[] valid = new bool[5];
             valid[0] = ValidateEmployee(name, lastName, socialInsuranceNumber, dateOfBirth);
-            valid[1] = ValidateDate(contractStartDate);
-            valid[2] = ValidateDate(contractStopDate);
+            if (valid[0])
+            {
+                this.dateOfBirth = Convert.ToDateTime(dateOfBirth);
+            }
+            valid[1] = ValidateDate(contractStartDate, dateType.CONTRACT_START);
+            valid[2] = ValidateDate(contractStopDate, dateType.CONTRACT_END);
             valid[3] = ValidateMoney(fixedContractAmount);
             if (valid[0] & valid[1] & valid[2] & valid[3])
             {
@@ -42,5 +52,44 @@ namespace AllEmployees
             }
             return allValid;
         }
+
+        protected virtual bool ValidateDate(string date, dateType type)
+        {
+            bool valid = false;
+            CultureInfo culture;
+            culture = CultureInfo.CreateSpecificCulture("en-US");
+            string[] formats = { "yyyy/MM/dd", "yyyy/M/dd" };
+            DateTime dateValue;
+
+            if (DateTime.TryParseExact(date, formats, new CultureInfo("en-US"), DateTimeStyles.None, out dateValue))
+            {
+                switch (type)
+                {
+                    case dateType.BIRTH:
+                        if (dateValue <= DateTime.Now)
+                        {
+                            valid = true;
+                            dateOfBirth = dateValue;
+                        }
+                        break;
+                    case dateType.CONTRACT_START:
+                        if (dateValue >= dateOfBirth && dateValue <= DateTime.Now)
+                        {
+                            valid = true;
+                            contractStartDate = dateValue;
+                        }
+                        break;
+                    case dateType.CONTRACT_END:
+                        if (dateValue >= contractStartDate && dateValue <= DateTime.Now)
+                        {
+                            valid = true;
+                            contractStopDate = dateValue;
+                        }
+                        break;
+                }
+            }
+            return valid;
+        }
+
     }
 }
