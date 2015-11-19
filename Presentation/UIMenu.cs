1d3082d497d2c907a253f6196053e3047bf2267b
Steven johnston <wheatthins@hotmail.ca>
2015-11-18 22:18:04 -0500

    Added some create employee content in menu ui

--- Presentation/UIMenu.cs
+++ Presentation/UIMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using TheCompany;
 namespace Presentation
@@ -11,7 +12,7 @@ namespace Presentation
     {
         public delegate nextFunction nextFunction();
         EmployeeDirectory employeeDirectory = new EmployeeDirectory();
-
+        bool newEmployee = false;
         public void Launch()
         {
             nextFunction lastFunc = null;
@@ -119,10 +120,12 @@ namespace Presentation
                     break;
                 case "2":
                     Console.Clear();
+                    newEmployee = true;
                     next = EmployeeDetailsMenu;
                     break;
                 case "3":
                     Console.Clear();
+                    newEmployee = false;
                     next = EmployeeDetailsMenu;
                     break;
                 case "4":
@@ -171,12 +174,45 @@ namespace Presentation
         }
         public nextFunction EmployeeDetailsMenu()
         {
+            string menuChoice;
+            string[] employeeInfo = new string[10];
+            nextFunction next = null;
             Console.WriteLine("Menu 4 : EMPLOYEE DETAILS MENU ");
             Console.WriteLine("1. Specify Base Employee Details");
             Console.WriteLine("2. Manage Employees");
             Console.WriteLine("9. Return to Employee Management Menu");
-            return null;
-        }
+            menuChoice = Console.ReadLine();
+            switch (menuChoice)
+            {
+                case "1":
+                    //InputTillCorrect(new Regex[] { new Regex(@"^FT$"), new Regex(@"^PT$"), new Regex(@"^CT$"), new Regex(@"^SS$") },"Enter Employee Type", "Employee Types consist of (FT,PT,CT,SS,)");
+                    employeeInfo[0] = InputTillCorrect(new Regex(@"^(?i)(FT|PT|CT|SS|Full Time|Part Time|Contract|Seasonal)(?-i)$"), "Enter Employee Type", "Employee Types consist of (FT,PT,CT,SS,)");
+                    employeeInfo[1] = InputTillCorrect(new Regex(@"^w+$"), "Enter Employee Last Name", "Employee Last Name consist of only characters");
+                    employeeInfo[2] = InputTillCorrect(new Regex(@"^w+$"), "Enter Employee First Name", "Employee First Name consist of only characters");
 
+                    if(employeeInfo[0] == )
+                    employeeInfo[3] = InputTillCorrect(new Regex(@"^$"), "Enter Employee ", "Employee  consist of ()");
+                    employeeInfo[4] = InputTillCorrect(new Regex(@"^$"), "Enter Employee ", "Employee  consist of ()");
+                    employeeInfo[5] = InputTillCorrect(new Regex(@"^$"), "Enter Employee ", "Employee  consist of ()");
+                    break;
+                case "2":
+                    break;
+                case "9":
+                    break;
+            }
+            return next;
+        }
+        public string InputTillCorrect(Regex regex,string message, string errorMessage)
+        {
+            string lineIn = "";
+            //for(;(from oneRegex in regex where oneRegex.IsMatch(lineIn) == true select oneRegex).Count() == 0; lineIn = Console.ReadLine())
+            for(;!regex.IsMatch(lineIn);lineIn = Console.ReadLine())
+            {
+                Console.Clear();
+                Console.WriteLine(errorMessage);
+                Console.WriteLine(message);
+            }
+            return lineIn;
+        }
     }
 }
