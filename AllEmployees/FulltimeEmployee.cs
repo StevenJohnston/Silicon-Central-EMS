48b2c5a0e1827e744c67838c53bebaaf6f8c3264
Matthew Warren <warren.matt@hotmail.com>
2015-11-19 13:31:33 -0500

    Added ValidateDate, works for dateOfBirth, dateOfHire, and dateOfTermination

--- AllEmployees/FulltimeEmployee.cs
+++ AllEmployees/FulltimeEmployee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,8 @@ namespace AllEmployees
 {
     public class FulltimeEmployee : Employee
     {
-        string dateOfHire;
-        string dateOfTermination;
+        DateTime dateOfHire;
+        DateTime dateOfTermination;
         decimal salary;
         public FulltimeEmployee(string[] employeeData)
         {
@@ -20,28 +21,12 @@ namespace AllEmployees
                 firstName = employeeData[index];
                 lastName = employeeData[index + 1];
                 socialInsuranceNumber = employeeData[index + 2];
-                dateOfBirth = employeeData[index + 3];
-                dateOfHire = employeeData[index + 4];
-                dateOfTermination = employeeData[index + 5];
+                dateOfBirth = Convert.ToDateTime(employeeData[index + 3]);
+                dateOfHire = Convert.ToDateTime(employeeData[index + 4]);
+                dateOfTermination = Convert.ToDateTime(employeeData[index + 5]);
                 salary = Convert.ToDecimal(employeeData[index + 6]);
             }
         }
-
-        public FulltimeEmployee(string name, string lastName, string socialInsuranceNumber, string dateOfBirth, string dateOfHire, string dateOfTermination, decimal salary)
-        {
-            if (ValidateFulltime(name, lastName, socialInsuranceNumber, dateOfBirth, dateOfHire, dateOfTermination, salary))
-            {
-                this.firstName = name;
-                this.lastName = lastName;
-                this.socialInsuranceNumber = socialInsuranceNumber;
-                this.dateOfBirth = dateOfBirth;
-                this.dateOfHire = dateOfHire;
-                this.dateOfTermination = dateOfTermination;
-                this.salary = salary;
-            }
-        }
-        
-
 
         private bool ValidateFulltime(string name, string lastName, string socialInsuranceNumber, string dateOfBirth, string dateOfHire, string dateOfTermination, decimal salary)
         {
@@ -49,8 +34,12 @@ namespace AllEmployees
             bool allValid = false;
 
             valid[0] = ValidateEmployee(name, lastName, socialInsuranceNumber, dateOfBirth);
-            valid[1] = ValidateDate(dateOfHire);
-            valid[2] = ValidateDate(dateOfTermination);
+            if (valid[0])
+            {
+                this.dateOfBirth = Convert.ToDateTime(dateOfBirth);
+            }
+            valid[1] = ValidateDate(dateOfHire, dateType.HIRE);
+            valid[2] = ValidateDate(dateOfTermination, dateType.TERMINATE);
             valid[3] = ValidateMoney(salary);
             if( valid[0] & valid[1] & valid[2] & valid[3])
             {
@@ -58,6 +47,46 @@ namespace AllEmployees
             }
             return allValid;
         }
+
+        protected bool ValidateDate(string date, dateType type)
+        {
+            bool valid = false;
+            CultureInfo culture;
+            culture = CultureInfo.CreateSpecificCulture("en-US");
+            string[] formats = { "yyyy/MM/dd", "yyyy/M/dd" };
+            DateTime dateValue;
+
+            if (DateTime.TryParseExact(date, formats, new CultureInfo("en-US"), DateTimeStyles.None, out dateValue))
+            {
+                switch (type)
+                {
+                    case dateType.BIRTH:
+                        if (dateValue <= DateTime.Now)
+                        {
+                            valid = true;
+                            dateOfBirth = dateValue;
+                        }
+                        break;
+                    case dateType.HIRE:
+                        if (dateValue >= dateOfBirth && dateValue <= DateTime.Now)
+                        {
+                            valid = true;
+                            dateOfHire = dateValue;
+                        }
+                        break;
+                    case dateType.TERMINATE:
+                        if (dateValue >= dateOfHire && dateValue <= DateTime.Now)
+                        {
+                            valid = true;
+                            dateOfTermination = dateValue;
+                        }
+                        break;
+                }
+            }
+            return valid;
+        }
+        
+        
 
 
         public override string ToString()
