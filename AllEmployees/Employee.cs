db6a8573a8b530acb35bdef2e1c2c289e7c3179d
Matthew Warren <warren.matt@hotmail.com>
2015-11-19 10:02:58 -0500

    Added ValidateEmployee, ValdiateSIN, ValidateName. Changed ValidateDateOfBirth to ValidateDate and made it return bool. Fixed constructor to call validate functions.

--- AllEmployees/Employee.cs
+++ AllEmployees/Employee.cs
@@ -4,11 +4,14 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace AllEmployees
 {
     public class Employee
     {
+        public static int[] sinCheck = new int[9] { 1, 2, 1, 2, 1, 2, 1, 2, 1 };
+
         protected string firstName;
         protected string lastName;
         private string socialInsuranceNumber;
@@ -56,18 +59,85 @@ namespace AllEmployees
         }
         public Employee(string firstName, string lastName, string socialInsuranceNumber, string dateOfBirth)
         {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.SocialInsuranceNumber = socialInsuranceNumber;
-            this.dateOfBirth = dateOfBirth;
-
+            if (ValidateEmployee(firstName, lastName, socialInsuranceNumber, dateOfBirth))
+            {
+                this.firstName = firstName;
+                this.lastName = lastName;
+                this.SocialInsuranceNumber = socialInsuranceNumber;
+                this.dateOfBirth = dateOfBirth;
+            }
         }
-        string ValidateDateOfBirth(string dateOfBirth)
+
+
+        public bool ValidateEmployee(string name, string lastName, string socialInsuranceNumber, string dateOfBirth)
         {
+            bool[] valid = new bool[4] { false, false, false, false };
+            bool allValid = false;
+            valid[0] = ValidateName(name);
+            valid[1] = ValidateName(lastName);
+            valid[2] = ValidateSIN(socialInsuranceNumber);
+            valid[3] = ValidateDate(dateOfBirth);
+            if(valid[0] & valid[1] & valid[2] & valid[3])
+            {
+                allValid = true;
+            }
+            return allValid;
+        }
+
+        private bool ValidateName(string name)
+        {
+            return Regex.IsMatch(name, "^[a-zA-Z'-]*?$");
+        }
+
+        public bool ValidateSIN(string socialInsuranceNumber)
+        {
+            //first digit can't be 0 or 8
+            int newSin = 0;
+            Int32.TryParse(socialInsuranceNumber.Replace(" ", string.Empty), out newSin);
+
+            int[] tempSin = new int[9];
+            int[] sin = new int[9];
+            double totalSin = 0;
+            bool validSin = false;
+            int toAdd = 0;
+            int theSin = newSin;
+            for (int x = 8; x >= 0; x--) //splits the SIN into an array
+            {
+                sin[x] = newSin % 10;
+                newSin /= 10;
+            }
+
+            if (sin[0] != 8 && sin[0] >= 1)
+            {
+                for (int x = 0; x < 8; x++)
+                {
+                    toAdd = sinCheck[x] * sin[x];
+                    if (toAdd >= 10)
+                    {
+                        toAdd = (toAdd % 10) + (toAdd / 10);
+                    }
+                    totalSin += toAdd;
+                }
+
+                if ((Math.Ceiling(totalSin / 10) * 10 - totalSin) == sin[8])
+                {
+                    validSin = true;
+                }
+            }
+            else if (theSin == 0)
+            {
+                validSin = true;
+            }
+            return validSin;
+        }
+        
+        private bool ValidateDate(string date)
+        {
+            bool valid = false;
             int year = 0;
             CultureInfo culture;
             culture = CultureInfo.CreateSpecificCulture("en-US");
-            string newTemp = dateOfBirth.ToLower();
+            string newTemp = date.ToLower();
             string formats = "yyyy/mm/dd";
             DateTime dateValue;
 
@@ -79,20 +149,19 @@ namespace AllEmployees
                 //check year range
                 if ((year >= 1900) && (year <= 2001))
                 {
-
+                    valid = true;
                 }
                 else
                 {
                     //put error code
                 }
-
             }
             else
             {
                 //put error code
             }
             //did not return anything because error code has not been created
-            return "";
+            return valid;
 
         }
     }
