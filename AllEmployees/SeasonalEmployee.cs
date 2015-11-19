5f18c6f166015bd858c11c4201690aea71785663
Matthew Warren <warren.matt@hotmail.com>
2015-11-19 10:04:32 -0500

    Added ValdiateSeason

--- AllEmployees/SeasonalEmployee.cs
+++ AllEmployees/SeasonalEmployee.cs
@@ -16,7 +16,33 @@ namespace AllEmployees
             summer,
             fall,
         }
-        seasons season;
-        double piecePay;
+        seasons season { get; set; }
+        Decimal piecePay { get; set; }
+        
+
+        private bool ValidateSeason(string newSeason)
+        {
+            bool valid = false;
+
+            if (newSeason.ToUpper() == "WINTER")
+            {
+                valid = true;
+            }
+            else if (newSeason.ToUpper() == "SPRING")
+            {
+                valid = true;
+            }
+            else if (newSeason.ToUpper() == "SUMMER")
+            {
+                valid = true;
+            }
+            else if (newSeason.ToUpper() == "FALL")
+            {
+                valid = true;
+            }
+
+            return valid;
+        }
+
     }
 }
