0570432dadf4729dd454d3a9347ee5d5aa23d168
Matthew Warren <warren.matt@hotmail.com>
2015-11-19 20:01:41 -0500

    Created LogString, which will write a string to the LogFile.txt in the Log folder. It will create both if they do not exist, and will automatically timestamp them.

--- Supporting/Logging.cs
+++ Supporting/Logging.cs
@@ -3,11 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Supporting
 {
-    public static class Logging
+    public class Logging
     {
-        
+        public static object lockObj = new object();
+
+        public static bool LogString(string toLog)
+        {
+            bool success = false;
+            string path = "Log\\LogFile.txt";
+            string temp = DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss") + ": ";
+            DateTime current = DateTime.Now;
+
+            try
+            {
+                if (!Directory.Exists("Log"))
+                {
+                    Directory.CreateDirectory("Log");
+                }
+                StreamWriter logFile = new StreamWriter(path, true);
+                logFile.Write(temp);
+                logFile.Write(toLog + "\n");
+                logFile.Close();
+                success = true;
+            }
+            catch
+            {
+                //error message thing here
+            }
+            return success;
+        }
+
+
     }
 }
