16f78443a36a43c33a57ac6884cf03f3e9e2c17a
Matthew Warren <warren.matt@hotmail.com>
2015-11-19 23:20:16 -0500

    Added VariablesLogString and SuccessLogString, these get called when the constructor is called. Will properly log errors/new parttime employees.

--- AllEmployees/ParttimeEmployee.cs
+++ AllEmployees/ParttimeEmployee.cs
@@ -12,6 +12,36 @@ namespace AllEmployees
         DateTime dateOfHire;
         DateTime dateOfTermination;
         decimal hourlyRate;
+        public bool VariablesLogString(string[] employeeData)
+        {
+            bool success = true;
+            int index = employeeData[0] == "PT" ? 1 : 0;
+            string toLog = "Trying to create Parttime Employee with:\n||\tFirst Name: " + employeeData[index] +
+                        "||Last Name: " + employeeData[index + 1] +
+                        "||SIN: " + employeeData[index + 2] +
+                        "||Date of Birth: " + employeeData[index + 3] +
+                        "||Date of Hire: " + employeeData[index + 4] +
+                        "||Date of Termination: " + employeeData[index + 5] +
+                        "||Hourly Rate: " + employeeData[index + 6];
+            AddToLogString(toLog);
+            return success;
+        }
+
+        public bool SuccessLogString()
+        {
+            bool success = true;
+            if (IsValid)
+            {
+                AddToLogString("\t-->Creating Parttime Employee was successful.");
+            }
+            else
+            {
+                AddToLogString("\t-->Creating Parttime Employee failed.");
+            }
+            Supporting.Logging.LogString(logString);
+            return success;
+        }
+
         public ParttimeEmployee()
         {
 
@@ -19,7 +49,7 @@ namespace AllEmployees
         public ParttimeEmployee(string[] employeeData)
         {
             int index = employeeData[0] == "PT" ? 1 : 0;
-
+            VariablesLogString(employeeData);
             if (ValidateParttime(employeeData[index], employeeData[index + 1], employeeData[index + 2], employeeData[index + 3], employeeData[index + 4], employeeData[index + 5], Convert.ToDecimal(employeeData[index+6])))
             {
                 firstName = employeeData[index];
@@ -29,7 +59,9 @@ namespace AllEmployees
                 dateOfHire = Convert.ToDateTime(employeeData[index + 4]);
                 dateOfTermination = Convert.ToDateTime(employeeData[index + 5]);
                 hourlyRate = Convert.ToDecimal(employeeData[index + 6]);
+                IsValid = true;
             }
+            SuccessLogString();
         }
 
         private bool ValidateParttime(string name, string lastName, string socialInsuranceNumber, string dateOfBirth, string dateOfHire, string dateOfTermination, decimal hourlyRate)
@@ -64,18 +96,22 @@ namespace AllEmployees
             {
                 switch (type)
                 {
-                    case dateType.BIRTH:
-                        if (dateValue <= DateTime.Now)
-                        {
-                            valid = true;
-                            dateOfBirth = dateValue;
-                        }
-                        break;
                     case dateType.HIRE:
                         if (dateValue >= dateOfBirth && dateValue <= DateTime.Now)
                         {
                             valid = true;
                             dateOfHire = dateValue;
+                        }
+                        else
+                        {
+                            if (dateValue <= dateOfBirth)
+                            {
+                                AddToLogString("\tDate of Hire Error: Must be after the employee was born.");
+                            }
+                            else
+                            {
+                                AddToLogString("\tDate of Hire Error: Must be before the current date.");
+                            }
                         }
                         break;
                     case dateType.TERMINATE:
@@ -84,7 +120,29 @@ namespace AllEmployees
                             valid = true;
                             dateOfTermination = dateValue;
                         }
+                        else
+                        {
+                            if (dateValue <= dateOfHire)
+                            {
+                                AddToLogString("\tDate of Termination Error: Must be after the employee was born.");
+                            }
+                            else
+                            {
+                                AddToLogString("\tDate of Termination Error: Must be before the current date.");
+                            }
+                        }
                         break;
+                }
+            }
+            else
+            {
+                if (type == dateType.HIRE)
+                {
+                    AddToLogString("\tDate of Hire Error: Invalid format.\n||\t\tTried: " + date);
+                }
+                else if (type == dateType.TERMINATE)
+                {
+                    AddToLogString("\tDate of Termination Error: Invalid format.\n||\t\tTried: " + date);
                 }
             }
             return valid;
